33da6ca7a9862e5124ec2bdec9cef016df482d3e
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-21 17:04:46 -0300

    feat: Login.js  added

--- projeto14-mywallet-front/src/App.js
+++ projeto14-mywallet-front/src/App.js
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
 import { Login, SignUp, Transfers, Entry, Exit } from "./pages";
 
-export const pathsWithoutHeaderAndMenu = ['/', '/sign-up'];
+export const pathsWithoutHeaderAndMenu = ['/sign-in', '/sign-up'];
 
 export default function App() {
   return (
     <UserProvider>
          <BrowserRouter>
              <Routes>
-                 <Route path="/" element={<Login />} />
+                 <Route path="/sign-in" element={<Login />} />
                   <Route path="/sign-up" element={<SignUp />} />
                   <Route path="/transfers" element={<Transfers />} />
                   <Route path="/entry" element={<Entry />} />
